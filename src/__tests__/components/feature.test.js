4f6df6ae358391dd972b040ee2744498760e0d9b
Bukovski <kmanankov@bk.ru>
2021-11-11 11:24:00 +0300

    add: test form component

--- src/__tests__/components/feature.test.js
+++ src/__tests__/components/feature.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Feature } from '../../components';
 
+
 describe('<Feature />', () => {
   it('renders the <Feature /> with populated data', () => {
     const { container, getByText } = render(
@@ -13,6 +14,7 @@ describe('<Feature />', () => {
 
     expect(getByText('Unlimited films, TV programmes and more.')).toBeTruthy();
     expect(getByText('Watch anywhere. Cancel at any time.')).toBeTruthy();
+    
     expect(container.firstChild).toMatchSnapshot();
   });
 
