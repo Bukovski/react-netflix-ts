ae26d3dd301a7ad5104a156edcf14adc43401268
Bukovski <kmanankov@bk.ru>
2021-11-12 10:20:00 +0300

    add: test home page

---
+++ src/__tests__/pages/home.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { render } from '@testing-library/react';
+import { Home } from '../../pages';
+
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+  useParams: jest.fn(),
+}));
+
+test('renders the homepage', () => {
+  const { getByText, getAllByText, getAllByPlaceholderText } = render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  
+  expect(getByText('Unlimited films, TV programmes and more.')).toBeTruthy();
+  expect(getByText('Watch anywhere. Cancel at any time.')).toBeTruthy();
+  expect(getAllByPlaceholderText('Email address')).toBeTruthy();
+  expect(getAllByText('Try it now')).toBeTruthy();
+  expect(getAllByText('Ready to watch? Enter your email to create or restart your membership.')).toBeTruthy();
+});
